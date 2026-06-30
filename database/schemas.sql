d18a2856dcda53eddae535c46d7512cd2556f262
DiegoA-CR <diego.a.calvarior24@gmail.com>
2026-06-30 01:13:27 -0600

    actualizacion 30062026

--- database/schemas.sql
+++ database/schemas.sql
@@ -1,32 +1,41 @@
-CREATE TABLE elementos (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    numero_atomico INT NOT NULL UNIQUE,
-    simbolo VARCHAR(3) NOT NULL UNIQUE,
-    nombre VARCHAR(50) NOT NULL,
-    masa_atomica DECIMAL(10,6),
-    configuracion_electronica VARCHAR(30),
-    electronegatividad DECIMAL(4,2),
-    radio_atomico INT, -- en picómetros
-    estado_oxidacion VARCHAR(20),
-    año_descubrimiento INT
+SET search_path TO Tabla_Elementos;
+SHOW search_path;
+
+SELECT schema_name
+FROM information_schema.schemata;
+
+CREATE TABLE "Tabla_Elementos".elementos(
+	id_elemento SERIAL PRIMARY KEY ,
+	numero_atomico INT NOT NULL UNIQUE,
+	simbolo VARCHAR(3) NOT NULL UNIQUE,
+	nombre VARCHAR(50) NOT NULL,
+	masa_atomica DECIMAL(10, 6),
+	configuracion_electronica VARCHAR(30),
+	electronegatividad DECIMAL(4,2),
+	radio_atomico INT,
+	estado_oxidacion VARCHAR(20),
+	año_descubrimiento INT
 );
+
+SELECT * FROM Tabla_Elementos.elementos;
+
 
 -- Categorías de elementos
 CREATE TABLE categorias (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id PRIMARY KEY SERIAL,
     nombre VARCHAR(30) NOT NULL UNIQUE, -- 'Metal alcalino', 'Gas noble', etc.
     descripcion TEXT
 );
 
 -- Estados de la materia
 CREATE TABLE estados (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id PRIMARY KEY SERIAL,
     nombre VARCHAR(20) NOT NULL UNIQUE -- 'Sólido', 'Líquido', 'Gas', 'Desconocido'
 );
 
 -- Tipos de orbitales
 CREATE TABLE bloques (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id PRIMARY KEY SERIAL,
     letra CHAR(1) NOT NULL UNIQUE -- 's', 'p', 'd', 'f'
 );
 
